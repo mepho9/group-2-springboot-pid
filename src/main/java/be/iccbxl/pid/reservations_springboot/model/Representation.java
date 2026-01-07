9a72ec11f6bccef47f5fbaaaedb88ecae4cc142f
daniel resende <resende_daniel@live.be>
2026-01-07 14:11:49 +0100

    chore: migrate Representation to jakarta.persistence and fix imports

--- src/main/java/be/iccbxl/pid/reservations_springboot/model/Representation.java
+++ src/main/java/be/iccbxl/pid/reservations_springboot/model/Representation.java
@@ -1,39 +1,43 @@
 package be.iccbxl.pid.reservations_springboot.model;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.Table;
-import javax.persistence.Column;
-
-import com.github.slugify.Slugify;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 
 @Entity
-@Table(name="representations")
+@Table(name = "representations")
 public class Representation {
+
     @Id
-    @GeneratedValue(strategy=GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
     @ManyToOne
-    @JoinColumn(name="show_id", nullable=false)
+    @JoinColumn(name = "show_id", nullable = false)
     private Show show;
 
     /**
-     * Date de création de la représentation
+     * Date de la représentation
      */
+    @Column(name = "schedule")
     private LocalDateTime when;
 
     /**
      * Lieu de prestation de la représentation
      */
     @ManyToOne
-    @JoinColumn(name="location_id", nullable=true)
+    @JoinColumn(name = "location_id")
     private Location location;
 
     @ManyToMany
@@ -43,13 +47,16 @@ public class Representation {
             inverseJoinColumns = @JoinColumn(name = "user_id"))
     private List<User> users = new ArrayList<>();
 
-
-    public Representation() { }
+    public Representation() {}
 
     public Representation(Show show, LocalDateTime when, Location location) {
         this.show = show;
         this.when = when;
         this.location = location;
+    }
+
+    public Long getId() {
+        return id;
     }
 
     public Show getShow() {
@@ -76,37 +83,29 @@ public class Representation {
         this.location = location;
     }
 
-    public Long getId() {
-        return id;
-    }
-
     public List<User> getUsers() {
         return users;
     }
 
     public Representation addUser(User user) {
-        if(!this.users.contains(user)) {
+        if (!this.users.contains(user)) {
             this.users.add(user);
             user.addRepresentation(this);
         }
-
         return this;
     }
 
     public Representation removeUser(User user) {
-        if(this.users.contains(user)) {
+        if (this.users.contains(user)) {
             this.users.remove(user);
             user.getRepresentations().remove(this);
         }
-
         return this;
     }
-
 
     @Override
     public String toString() {
         return "Representation [id=" + id + ", show=" + show + ", when=" + when
                 + ", location=" + location + "]";
     }
-
 }
