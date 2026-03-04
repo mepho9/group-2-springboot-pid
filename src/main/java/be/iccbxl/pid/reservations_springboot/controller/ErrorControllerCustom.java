6cf166a3063eea59c5b5ed2867185dfc94160241
daniel resende <resende_daniel@live.be>
2026-03-04 18:33:47 +0100

    feat: add custom 403 error page

---
+++ src/main/java/be/iccbxl/pid/reservations_springboot/controller/ErrorControllerCustom.java
@@ -0,0 +1,13 @@
+package be.iccbxl.pid.reservations_springboot.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ErrorControllerCustom {
+
+    @GetMapping("/403")
+    public String accessDenied() {
+        return "error/403";
+    }
+}
